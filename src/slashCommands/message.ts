f6924fddc97d4843fda11b7896477cdf6bc6a82c
SamiBenamar <sami.benamar@viacesi.fr>
2023-02-17 14:40:58 +0100

    init/structure

---
+++ src/slashCommands/message.ts
@@ -0,0 +1,20 @@
+import { Message, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types";
+
+export const command: SlashCommand = {
+    name: 'message',
+    data: new SlashCommandBuilder()
+    .setName('message')
+    .setDescription('Envoie un message avec une réaction')
+    .addStringOption((option) => {
+        return option
+        .setName('message')
+        .setDescription('Message à afficher')
+        .setRequired(true);
+    }),
+    execute: async (interaction) => {
+        const message = interaction.options.get('message').value.toString();
+        await interaction.reply({ content: `Valeur du message : ${message}` });
+
+    }
+}
